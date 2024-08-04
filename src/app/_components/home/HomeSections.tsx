48c7c7889fac28f49970dec231a7c4ce72cdb1c1
Kevin Li <34748712+Kleanur@users.noreply.github.com>
2024-08-04 13:07:09 -0400

    WIP stamp element

--- src/app/_components/home/HomeSections.tsx
+++ src/app/_components/home/HomeSections.tsx
@@ -1,5 +1,6 @@
 // 'use client';
 
+import Image from "next/image";
 import { useRef } from "react";
 import {
   motion,
@@ -8,21 +9,28 @@ import {
 import { useParallax } from "@/app/_lib/motion";
 import SectionCard from "./SectionCard"
 import { dm_serif_display } from "@/app/_ui/fonts";
+import "@/app/_ui/stamp.css"
 
 export const IntroSection = () => {
     return (
         <SectionCard>
             <div className="absolute left-1/2 top-[16%] h-4/6 border-l-4 border-accent-rgb"></div>
             <div className="h-full w-full grid grid-cols-2 grid-rows-5">
-                <div className="col-span-1 row-span-5 m-2 border-black">
+                <div className="col-span-1 row-span-5 m-2 border border-black">
                     <h1 className={`text-5xl ${dm_serif_display.className}`}>
                         Hello World!
                     </h1>
                 </div>
-                <div className="col-span-1 row-span-2 m-2  border-black">
-
+                <div className="col-span-1 row-span-2 m-2 border border-black">
+                    <div className="stamp">
+                    {/* <Image 
+                        src="/tabletop-cropped-2.png"
+                        alt=""
+                        fill
+                    /> */}
+                    </div>
                 </div>
-                <div className="col-span-1 row-span-3 m-2  border-black">
+                <div className="col-span-1 row-span-3 m-2 border border-black">
 
                 </div>
             </div>
